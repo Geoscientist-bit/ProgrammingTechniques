7507bd11fb77dd9751f1c27ca45aa2a6be47bb0a
Jhon Edison Muñoz <83843516+Geoscientist-bit@users.noreply.github.com>
2023-09-15 17:31:48 -0500

    actividad 1

---
+++ individual_activity_1/src/DisponibilidadAgua.java
@@ -0,0 +1,53 @@
+/*
+ * Basado en la meta 6 de agua limpia y saneamiento se tomo 20 ubicaciones con disponilidad de agua 
+ * de los cuales determino el promedio de agua que generan estas ubicaciones, la ubicacicón con mayor disponibilidd
+ * y la de menor disponibilidad.
+ * 
+ * esto con el objetivo de determinar las ubcaciones que pueden satisfacer la demanda de mas de 100 personas, teniendo
+ * en cuenta que solo las ubicaciones que tenga disponibilidad mayor a 15 litros puede satisfacer esta necesidad
+ */
+
+public class DisponibilidadAgua {
+
+    public static void main(String[] args) {
+        // Crear un arreglo para representar la disponibilidad de agua en 20 ubicaciones
+        double[] disponibilidadAgua = new double[20];
+        double suma = 0.0 , promedio = 0.0;
+
+        // Llenar el arreglo con valores de disponibilidad de agua 
+        for (int i = 0; i < disponibilidadAgua.length; i++) {
+            disponibilidadAgua[i] = Math.random() * 100; // Valores aleatorios entre 0 y 100
+        }
+
+        // Imprimir la disponibilidad de agua en cada ubicación
+        System.out.println("Disponibilidad de agua en cada ubicación:");
+        System.out.println("------------------------------------------");
+        double mayor = disponibilidadAgua[0];
+        double menor = disponibilidadAgua[0];
+        for (int i = 0; i < disponibilidadAgua.length; i++) {
+            System.out.println("Ubicación " + (i + 1) + ": " + disponibilidadAgua[i] + " litros");
+
+            if (mayor < disponibilidadAgua[i]) {
+                mayor = disponibilidadAgua[i];
+            }
+
+            if (menor > disponibilidadAgua[i]) {
+                menor = disponibilidadAgua[i];
+            }
+
+            
+            suma += disponibilidadAgua[i];
+
+        }
+
+        promedio = suma / disponibilidadAgua.length;
+
+        System.out.println("mayor valor es " + mayor);
+
+        System.out.println("menor valor es " + menor);
+        
+        System.out.println("--> El promedio de disponidad de agua es: " + promedio);
+
+       
+    }
+}
